a35d1b40223bd27bdb0efdbbf870a39319e7fe8a
Javier Rodriguez <javierorodriguez@gmail.com>
2015-10-02 22:02:09 +0300

    Examples for #titleize and #blank?

--- food_finder/spec/support/string_extend_spec.rb
+++ food_finder/spec/support/string_extend_spec.rb
@@ -2,32 +2,50 @@ describe 'String' do
 
   describe "#titleize" do
 
-    it "capitalizes each word in a string"
-    
-    it "works with single-word strings"
-    
-    it "capitalizes all uppercase strings"
-    
-    it "capitalizes mixed-case strings"
-    
+    it "capitalizes each word in a string" do
+      expect("some sample text".titleize).to eq('Some Sample Text')
+    end
+
+    it "works with single-word strings" do
+      expect('ruby'.titleize).to eq('Ruby')
+    end
+
+    it "capitalizes all uppercase strings" do
+      expect('SOME SAMPLE TEXT'.titleize).to eq('Some Sample Text')
+    end
+
+    it "capitalizes mixed-case strings" do
+      expect('sOMe SaMPle TeXT'.titleize).to eq('Some Sample Text')
+    end
+
   end
-  
+
   describe '#blank?' do
 
-    it "returns true if string is empty"
+    it "returns true if string is empty" do
+      expect(''.blank?).to be true
+    end
 
-    it "returns true if string contains only spaces"
+    it "returns true if string contains only spaces" do
+      expect('    '.blank?).to be true
+    end
 
-    it "returns true if string contains only tabs"
-    # Get a tab using a double-quoted string with \t
-    # example: "\t\t\t"
+    it "returns true if string contains only tabs" do
+      expect("\t\t\t".blank?).to be true
+    end
 
-    it "returns true if string contains only spaces and tabs"
-    
-    it "returns false if string contains a letter"
+    it "returns true if string contains only spaces and tabs" do
+      expect(" \t \t  \t".blank?).to be true
+    end
 
-    it "returns false if string contains a number"
-    
+    it "returns false if string contains a letter" do
+      expect('a'.blank?).to be false
+    end
+
+    it "returns false if string contains a number" do
+      expect('0'.blank?).to be false
+    end
+
   end
-  
+
 end
