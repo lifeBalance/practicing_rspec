d91b5a9c48be14475ab4bcbaa858178509f5c60e
Javier Rodriguez <javierorodriguez@gmail.com>
2015-10-02 14:54:40 +0300

    Use let and before hook with spies

--- car_project/spec/doubles_spec.rb
+++ car_project/spec/doubles_spec.rb
@@ -214,4 +214,71 @@ describe 'Doubles' do
 
   end
 
+  context 'with spying abilities' do
+
+    it 'can expect a call after it is received' do
+      dbl = spy('Chant')
+      allow(dbl).to receive(:hey!).and_return('Hoo!')
+      dbl.hey!
+      expect(dbl).to have_received(:hey!)
+    end
+
+    it 'can use message constraints' do
+      dbl = spy('Chant')
+      allow(dbl).to receive(:hey!).and_return('Hoo!')
+      dbl.hey!
+      dbl.hey!
+      dbl.hey!
+      expect(dbl).to have_received(:hey!).with(no_args).exactly(3).times
+    end
+
+    it 'can expect any message already sent to a declared spy' do
+      customer = spy('Customer')
+      # No need to stub `send_invoice`
+      # allow(customer).to receive(:send_invoice)
+      customer.send_invoice
+      expect(customer).to have_received(:send_invoice)
+    end
+
+    it 'can expect only allowed messages on partial doubles' do
+      class Customer
+        def send_invoice
+          true
+        end
+      end
+      customer = Customer.new
+      # We need to stub a method. Can't spy on everything.
+      allow(customer).to receive(:send_invoice)
+      customer.send_invoice
+      expect(customer).to have_received(:send_invoice)
+    end
+
+  end
+
+  context 'using let and before hook' do
+    let(:order) do
+      spy('Order',  :process_line_items => nil,
+                    :charge_on_credit_card => true,
+                    :send_confirmation_email => true)
+    end
+
+    before(:example) do
+      order.process_line_items
+      order.charge_on_credit_card
+      order.send_confirmation_email
+    end
+
+    it 'calls #process_line_items on the order' do
+      expect(order).to have_received(:process_line_items)
+    end
+
+    it 'calls #charge_on_credit_card on the order' do
+      expect(order).to have_received(:charge_on_credit_card)
+    end
+
+    it 'calls #send_confirmation_email on the order' do
+      expect(order).to have_received(:send_confirmation_email)
+    end
+  end
+
 end
