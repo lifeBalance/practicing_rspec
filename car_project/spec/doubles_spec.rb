b489f6c97c39678ac7ffdaa53de5af97faf2cfe4
Javier Rodriguez <javierorodriguez@gmail.com>
2015-10-02 13:11:42 +0300

    Add argument constraints to message expectations

--- car_project/spec/doubles_spec.rb
+++ car_project/spec/doubles_spec.rb
@@ -135,4 +135,43 @@ describe 'Doubles' do
 
   end
 
+  context 'with argument constraints' do
+
+    it 'expects arguments will match' do
+      dbl = double('Customer list')
+      expect(dbl).to receive(:sort).with('name')
+      dbl.sort('name')
+    end
+
+    it 'passes when any arguments are allowed' do
+      dbl = double('Customer list')
+      # The default, same as not using `with()` at all.
+      expect(dbl).to receive(:sort).with(any_args)
+      dbl.sort('name')
+    end
+
+    it 'works the same with multiple arguments' do
+      dbl = double('Customer list')
+      expect(dbl).to receive(:sort).with('name', 'asc', true)
+      dbl.sort('name', 'asc', true)
+    end
+
+    it 'allows constraining only some arguments' do
+      dbl = double('Customer list')
+      expect(dbl).to receive(:sort).with('name', anything, anything)
+      dbl.sort('name', 'asc', true)
+    end
+
+    it 'allows using other matchers' do
+      dbl = double('Customer list')
+      expect(dbl).to receive(:sort).with(
+        a_string_starting_with('n'),
+        an_object_eq_to('asc') | an_object_eq_to('desc'),
+        boolean
+      )
+      dbl.sort('name', 'asc', true)
+    end
+
+  end
+
 end
