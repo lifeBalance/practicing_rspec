eedf5c234df305dccedab8755a2fa65be5cd0d97
Javier Rodriguez <javierorodriguez@gmail.com>
2015-10-02 23:01:11 +0300

    Add Restaurant examples

--- food_finder/spec/restaurant_spec.rb
+++ food_finder/spec/restaurant_spec.rb
@@ -3,25 +3,36 @@ require 'restaurant'
 describe Restaurant do
 
   let(:test_file) { 'spec/fixtures/restaurants_test.txt' }
-  let(:crescent) { Restaurant.new(:name => 'Crescent', :cuisine => 'paleo', :price => '321') }
-  
+  let(:crescent) { Restaurant.new(:name     => 'Crescent',
+                                  :cuisine  => 'paleo',
+                                  :price    => '321') }
+
   describe 'attributes' do
-  
-    it "allow reading and writing for :name"
 
-    it "allow reading and writing for :cuisine"
+    it "allow reading and writing for :name" do
+      subject.name = 'Test'
+      expect(subject.name).to eq('Test')
+    end
 
-    it "allow reading and writing for :price"
-    
+    it "allow reading and writing for :cuisine" do
+      subject.cuisine = 'Martian'
+      expect(subject.cuisine).to eq('Martian')
+    end
+
+    it "allow reading and writing for :price" do
+      subject.price = '123'
+      expect(subject.price).to eq('123')
+    end
+
   end
-  
+
   describe '.load_file' do
 
     it 'does not set @@file if filepath is nil' do
       no_output { Restaurant.load_file(nil) }
       expect(Restaurant.file).to be_nil
     end
-    
+
     it 'sets @@file if filepath is usable' do
       no_output { Restaurant.load_file(test_file) }
       expect(Restaurant.file).not_to be_nil
@@ -34,88 +45,141 @@ describe Restaurant do
         Restaurant.load_file(nil)
       end.to output(/not usable/).to_stdout
     end
-    
+
     it 'does not output a message if filepath is usable' do
       expect do
         Restaurant.load_file(test_file)
       end.not_to output.to_stdout
     end
-    
+
   end
-  
+
   describe '.all' do
-    
+
     it 'returns array of restaurant objects from @@file' do
-      pending
+      # pending
       Restaurant.load_file(test_file)
       restaurants = Restaurant.all
-      expect(restaurants.class).to eq('FIXME')
-      expect(restaurants.length).to eq('FIXME')
-      expect(restaurants.first.class).to eq('FIXME')
+      expect(restaurants.class).to eq(Array)
+      expect(restaurants.length).to eq(6)
+      expect(restaurants.first.class).to eq(Restaurant)
     end
 
     it 'returns an empty array when @@file is nil' do
-      pending
+      # pending
       no_output { Restaurant.load_file(nil) }
       restaurants = Restaurant.all
-      expect(restaurants).to eq('FIXME')
+      expect(restaurants).to eq([])
     end
-    
+
   end
-  
+
   describe '#initialize' do
 
     context 'with no options' do
       # subject would return the same thing
       let(:no_options) { Restaurant.new }
 
-      it 'sets a default of "" for :name'
+      it 'sets a default of "" for :name' do
+        expect(subject.name).to eq('')
+      end
 
-      it 'sets a default of "unknown" for :cuisine'
+      it 'sets a default of "unknown" for :cuisine' do
+        expect(subject.cuisine).to eq('unknown')
+      end
 
-      it 'does not set a default for :price'
+      it 'does not set a default for :price' do
+        expect(subject.price).to be_nil
+      end
     end
-    
+
     context 'with custom options' do
-      
-      it 'allows setting the :name'
+      # crescent already call #new using custom options
+      it 'allows setting the :name' do
+        expect(crescent.name).to eq('Crescent')
+      end
 
-      it 'allows setting the :cuisine'
+      it 'allows setting the :cuisine' do
+        expect(crescent.cuisine).to eq('paleo')
+      end
 
-      it 'allows setting the :price'
+      it 'allows setting the :price' do
+        expect(crescent.price).to eq('321')
+      end
 
     end
 
   end
-  
+
   describe '#save' do
-    
-    it 'returns false if @@file is nil'
-    
-    it 'returns false if not valid'
-    
-    it 'calls append on @@file if valid'
-    
+
+    it 'returns false if @@file is nil' do
+      # Don't load a file here
+      expect(Restaurant.file).to be_nil
+
+      expect(crescent.save).to be false
+    end
+
+    it 'returns false if not valid' do
+      Restaurant.load_file(test_file)
+      expect(Restaurant.file).not_to be_nil
+
+      # subject is invalid by default
+      expect(subject.save).to be false
+    end
+
+    it 'calls append on @@file if valid' do
+      Restaurant.load_file(test_file)
+      expect(Restaurant.file).not_to be_nil
+
+      # Message expectation on partial test double
+      expect(Restaurant.file).to receive(:append).with(crescent)
+      crescent.save
+    end
+
   end
-  
+
   describe '#valid?' do
-    
-    it 'returns false if name is nil'
 
-    it 'returns false if name is blank'
+    it 'returns false if name is nil' do
+      crescent.name = nil
+      expect(crescent.valid?).to be false
+    end
 
-    it 'returns false if cuisine is nil'
+    it 'returns false if name is blank' do
+      crescent.name = '   '
+      expect(crescent.valid?).to be false
+    end
 
-    it 'returns false if cuisine is blank'
-    
-    it 'returns false if price is nil'
+    it 'returns false if cuisine is nil' do
+      crescent.cuisine = nil
+      expect(crescent.valid?).to be false
+    end
 
-    it 'returns false if price is 0'
-    
-    it 'returns false if price is negative'
+    it 'returns false if cuisine is blank' do
+      crescent.cuisine = nil
+      expect(crescent.valid?).to be false
+    end
 
-    it 'returns true if name, cuisine, price are present'
-    
+    it 'returns false if price is nil' do
+      crescent.price = nil
+      expect(crescent.valid?).to be false
+    end
+
+    it 'returns false if price is 0' do
+      crescent.price = 0
+      expect(crescent.valid?).to be false
+    end
+
+    it 'returns false if price is negative' do
+      crescent.price = -10
+      expect(crescent.valid?).to be false
+    end
+
+    it 'returns true if name, cuisine, price are present' do
+      expect(crescent.valid?).to be true
+    end
+
   end
 
 end
